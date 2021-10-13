4f113d033be9f398bf866892ed3ed0f98be654c2
MattiaGentile <mattia.gentile.mail@gmail.com>
2021-10-13 13:16:35 +0200

    Esplorazione dataset

--- project.R
+++ project.R
@@ -27,9 +27,10 @@ south_n=length(which(area=='Southern'))
 dis_bord=unlist(Base[,12], use.names = FALSE)
 dis_grid=unlist(Base[,13], use.names = FALSE)
 
-boxplot(dis_grid)
+bp=boxplot(dis_grid)
 unlist(Base[which(dis_grid>200),3])
 unlist(Base[which(dis_grid>200),4])
+bp$out
 
 boxplot(dis_bord)
 unlist(Base[which(dis_bord>150),3])
@@ -76,6 +77,38 @@ points(demand[area=='West'],cost[area=='West'], col='Blue', pch=16)
 points(demand[area=='Central'],cost[area=='Central'], col='Green', pch=16)
 points(demand[area=='Southern'],cost[area=='Southern'], col='Gold', pch=16)
 abline(a=0, b=4000)
+
+
+### Classification of refugee settlement --> difference in population mainly and distance from border
+x11()
+plot(dis_bord, pop)
+points(dis_bord[type=='Refugee Camp'], pop[type=='Refugee Camp'], col='red', pch=16)
+points(dis_bord[type=='Refugee Settlement'], pop[type=='Refugee Settlement'], col='Blue', pch=16)
+points(dis_bord[type=='Refugee Location'], pop[type=='Refugee Location'], col='Green', pch=16)
+points(dis_bord[type=='Refugee Urban Location'], pop[type=='Refugee Urban Location'], col='Gold', pch=16)
+
+m_camp=mean(pop[type=='Refugee Camp'])
+m_settlement=mean(pop[type=='Refugee Settlement'])
+m_location=mean(pop[type=='Refugee Location'])
+m_urban=mean(pop[type=='Refugee Urban Location'])
+
+
+library(plyr)
+n_of_camps=count(Base$country)
+
+
+
+
+
+### DEPTH MEASURE ###
+
+library(MASS)
+library(rgl)
+library(DepthProc)
+library(hexbin)
+library(packagefinder)
+library(aplpack)
+library(robustbase)
 
 
 
